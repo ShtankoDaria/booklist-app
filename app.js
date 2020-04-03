458c9a33ef818f9e9ead7b50757e24365eb6ba50
ShtankoDaria <daria.shtanko@gmail.com>
2020-04-03 17:43:23 +0200

    Storage class

--- app.js
+++ app.js
@@ -54,3 +54,35 @@ class UI {
       document.querySelector('#isbn').value = '';
     }
   }
+
+  // Store Class: Storage of books
+class Store {
+    static getBooks() {
+      let books;
+      if(localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(isbn) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if(book.isbn === isbn) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
